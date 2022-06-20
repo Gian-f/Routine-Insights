b93472c57aacba81df4df0d47e9bcd5dc11b18ed
Gian Felipe <gianfelipe87@gmail.com>
2022-06-20 10:29:37 -0300

    Implementa navegação da SplashFragment para LoginFragment

--- app/src/main/java/com/example/todoapp/ui/SplashFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/SplashFragment.kt
@@ -1,19 +1,42 @@
 package com.example.todoapp.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSplashBinding
 
 class SplashFragment: Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::chechAuth, 4000)
+    }
+
+    private fun chechAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
