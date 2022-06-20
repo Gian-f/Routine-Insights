799d077ba49d8ad644eb096154d875b5746feb2e
Gian Felipe <gianfelipe87@gmail.com>
2022-06-20 08:12:33 -0300

    Adiciona gráfico de lifecycle e HomeFragment

--- app/src/main/java/com/example/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
