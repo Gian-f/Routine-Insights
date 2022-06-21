3b15637047e839a7d6ca9b90a7e934d70801adf4
Gian Felipe <gianfelipe87@gmail.com>
2022-06-21 11:37:01 -0300

    criação e configuração do binding no DoingFragment e adiciona tabLayout

--- app/src/main/java/com/example/todoapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/HomeFragment.kt
@@ -6,13 +6,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.todoapp.R
+import com.example.todoapp.adapter.ViewPagerAdapter
+import com.example.todoapp.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configLayout()
+    }
+
+    private fun configLayout() {
+        val adapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = adapter
+
+        adapter.addFragment(TodoFragment(), "A fazer")
+        adapter.addFragment(DoingFragment(), "Fazendo")
+        adapter.addFragment(DoneFragment(), "Concluídas")
+
+        binding.viewPager.offscreenPageLimit = adapter.itemCount
+
+        TabLayoutMediator(
+            binding.tabs, binding.viewPager
+        ) { tab, position ->
+            tab.text = adapter.getTitle(
+                position
+            )
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
