e9fb4f92f78d5135a9274a08e2542648115f2e5c
Gian Felipe <gianfelipe87@gmail.com>
2022-06-21 11:36:08 -0300

    adiciona métodos no viewPager

--- app/src/main/java/com/example/todoapp/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/todoapp/adapter/ViewPagerAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.todoapp.adapter
 
-class ViewPagerAdapter {
+import android.icu.text.CaseMap
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity):
+    FragmentStateAdapter(fragmentActivity) {
+
+    private val fragmentList: MutableList<Fragment> = ArrayList()
+    private val titleList: MutableList<String> = ArrayList()
+
+
+    fun getTitle(position: Int): String {
+        return titleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
 }
+
+
+
