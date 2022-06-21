b4fe9435f545389a001e1b73b8d582c3af3e3aad
Gian Felipe <gianfelipe87@gmail.com>
2022-06-21 11:38:21 -0300

    Ajuste no navController

--- app/src/main/java/com/example/todoapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/auth/RegisterFragment.kt
@@ -60,7 +60,7 @@ class RegisterFragment : Fragment() {
         auth.createUserWithEmailAndPassword(email, senha)
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
-                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                    findNavController().navigate(R.id.action_global_homeFragment)
                 } else {
                     binding.progressBar.isVisible = false
                 }
