29a72dd1900af630d7117353c6cbb7c3946f1ad9
Gian Felipe <gianfelipe87@gmail.com>
2022-06-22 21:21:44 -0300

    adiciona navegação

--- app/src/main/java/com/example/todoapp/ui/TodoFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/TodoFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentTodoBinding
 
 class TodoFragment : Fragment() {
 
-    private var _binding : FragmentTodoBinding? = null
+    private var _binding: FragmentTodoBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,6 +22,18 @@ class TodoFragment : Fragment() {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
