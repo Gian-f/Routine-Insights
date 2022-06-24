9a10dd5fd4e94d9ab5e7314267d34cbd49af7eb9
Gian Felipe <gianfelipe87@gmail.com>
2022-06-23 21:29:06 -0300

    implementa camada de entidade

--- app/src/main/java/com/example/todoapp/model/Task.kt
+++ app/src/main/java/com/example/todoapp/model/Task.kt
@@ -1,3 +1,17 @@
 package com.example.todoapp.model
 
-data class Task()
+import android.os.Parcelable
+import com.example.todoapp.helper.FirebaseHelper
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Task(
+    var id: String = "",
+    var title: String = "",
+    var status: Int = 0
+) : Parcelable {
+    init {
+        this.id = FirebaseHelper.getDatabase().push().key ?: ""
+    }
+
+}
