2182cbc637703b0e7db8ff241c90a0f40269e6f9
Gian Felipe <gianfelipe87@gmail.com>
2022-06-23 21:29:36 -0300

    Cria TaskAdapter

--- app/src/main/java/com/example/todoapp/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/todoapp/ui/adapter/TaskAdapter.kt
@@ -1,11 +1,29 @@
-package com.example.todoapp.adapter
+package com.example.todoapp.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.R
 import com.example.todoapp.databinding.ItemAdapterBinding
+import com.example.todoapp.model.Task
 
-class TaskAdapter: RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
+
+class TaskAdapter(
+    private val context: Context,
+    private val taskList: List<Task>,
+    val taskSelected:(Task, Int) -> Unit
+): RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
+
+    companion object {
+        val SELECT_BACK: Int = 1
+        val SELECT_REMOVE: Int = 2
+        val SELECT_EDIT: Int = 3
+        val SELECT_DETAILS: Int = 4
+        val SELECT_NEXT: Int = 5
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapter.MyViewHolder {
         return MyViewHolder(
@@ -21,10 +39,44 @@ class TaskAdapter: RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
             RecyclerView.ViewHolder(binding.root)
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val task = taskList[position]
 
+        holder.binding.textTitle.text = task.title
+
+        holder.binding.btnDelete.setOnClickListener{taskSelected(task, SELECT_REMOVE)}
+        holder.binding.btnEdit.setOnClickListener{taskSelected(task, SELECT_EDIT)}
+        holder.binding.btnDetails.setOnClickListener{taskSelected(task, SELECT_DETAILS)}
+
+        when(task.status) {
+            0 -> {
+                holder.binding.ibBack.isVisible = false
+
+                holder.binding.ibNext.setColorFilter(
+                    ContextCompat.getColor(context, R.color.color_doing)
+                )
+
+                holder.binding.ibNext.setOnClickListener {taskSelected(task, SELECT_NEXT)}
+            }
+            1 -> {
+                holder.binding.ibBack.setColorFilter(
+                    ContextCompat.getColor(context, R.color.color_todo)
+                )
+
+                holder.binding.ibNext.setColorFilter(
+                    ContextCompat.getColor(context, R.color.color_done)
+                )
+
+                holder.binding.ibBack.setOnClickListener {taskSelected(task, SELECT_BACK)}
+                holder.binding.ibNext.setOnClickListener {taskSelected(task, SELECT_NEXT)}
+            }
+            else -> {
+                holder.binding.ibBack.setColorFilter(
+                    ContextCompat.getColor(context, R.color.color_doing)
+                )
+                holder.binding.ibBack.setOnClickListener {taskSelected(task, SELECT_BACK)}
+            }
+        }
     }
 
-    override fun getItemCount(): Int {
-
-    }
+    override fun getItemCount() = taskList.size
 }
