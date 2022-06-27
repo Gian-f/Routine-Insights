d55ef5c2efc770ddba4e027d72415cc07bbe06e5
Gian Felipe <gianfelipe87@gmail.com>
2022-06-27 18:17:26 -0300

    ajuste em refererência a camada de model

--- app/src/main/java/com/example/todoapp/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/todoapp/ui/adapter/TaskAdapter.kt
@@ -41,7 +41,7 @@ class TaskAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val task = taskList[position]
 
-        holder.binding.textTitle.text = task.title
+        holder.binding.textDescription.text = task.description
 
         holder.binding.btnDelete.setOnClickListener{taskSelected(task, SELECT_REMOVE)}
         holder.binding.btnEdit.setOnClickListener{taskSelected(task, SELECT_EDIT)}
