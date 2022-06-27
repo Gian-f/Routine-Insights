f007f16e77459600cf4210bbc18e4f8ec2df34b4
Gian Felipe <gianfelipe87@gmail.com>
2022-06-27 18:15:47 -0300

    atualiza camada de model

--- app/src/main/java/com/example/todoapp/model/Task.kt
+++ app/src/main/java/com/example/todoapp/model/Task.kt
@@ -7,7 +7,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Task(
     var id: String = "",
-    var title: String = "",
+    var description: String = "",
     var status: Int = 0
 ) : Parcelable {
     init {
