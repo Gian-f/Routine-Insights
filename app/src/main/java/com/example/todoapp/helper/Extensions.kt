55194f9805f49f00fdbb67a47485115c1ac54855
Gian Felipe <gianfelipe87@gmail.com>
2022-06-29 13:17:00 -0300

    Cria função extensível

--- app/src/main/java/com/example/todoapp/helper/Extensions.kt
+++ app/src/main/java/com/example/todoapp/helper/Extensions.kt
@@ -1,2 +1,15 @@
 package com.example.todoapp.helper
 
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+
+
+fun Fragment.initToolbar(toolbar: Toolbar) {
+    (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    (activity as AppCompatActivity).title = ""
+    (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    toolbar.setNavigationOnClickListener { activity?.onBackPressed() }
+    toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"))
+}
