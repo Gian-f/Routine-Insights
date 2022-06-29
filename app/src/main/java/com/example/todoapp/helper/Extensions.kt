2b652e7648d9e35592c96c690af62887cb258fa7
Gian Felipe <gianfelipe87@gmail.com>
2022-06-29 14:27:31 -0300

    Cria classe extensiva showBottomSheet

--- app/src/main/java/com/example/todoapp/helper/Extensions.kt
+++ app/src/main/java/com/example/todoapp/helper/Extensions.kt
@@ -4,6 +4,9 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.example.todoapp.R
+import com.example.todoapp.databinding.BottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 fun Fragment.initToolbar(toolbar: Toolbar) {
@@ -12,4 +15,26 @@ fun Fragment.initToolbar(toolbar: Toolbar) {
     (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
     toolbar.setNavigationOnClickListener { activity?.onBackPressed() }
     toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"))
+}
+
+fun Fragment.showBottomSheet(
+    titleDialog: Int? = null,
+    titleButton: Int? = null,
+    message: Int,
+    onClick: () -> Unit = {}
+) {
+    val bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+    val bottomSheetBinding: BottomSheetBinding =
+        BottomSheetBinding.inflate(layoutInflater, null, false)
+
+    bottomSheetBinding.textTitle.text = getString(titleDialog ?: R.string.text_title_bottom_sheet)
+    bottomSheetBinding.textMessage.text = getText(message)
+    bottomSheetBinding.btnClick.text = getString(titleButton ?: R.string.text_button_bottom_sheet)
+    bottomSheetBinding.btnClick.setOnClickListener {
+        onClick()
+        bottomSheetDialog.dismiss()
+    }
+
+    bottomSheetDialog.setContentView(bottomSheetBinding.root)
+    bottomSheetDialog.show()
 }
